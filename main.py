cc22c85b3d6db8139d0e01863e15b4a5b0232c95
brianpompey <brianthescholar@gmail.com>
2021-05-07 17:24:29 -0400

    changed spreadsheet name

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import gspread
 gc = gspread.service_account('credentials.json')
 
 # Open a sheet from a spreadsheet in one go
-wks = gc.open("Where is the money Lebowski?").sheet1
+wks = gc.open("tester-spreadsheet").sheet1
 
 # Update a range of cells using the top left corner address
 wks.update('A1', [[1, 2], [3, 4]])
