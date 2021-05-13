fcd2c3778917317e5017384c61e27a814c0955fa
brianpompey <brianthescholar@gmail.com>
2021-05-12 20:59:48 -0400

    changed text

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ wks = gc.open("tester-spreadsheet").sheet1
 wks.update('A1', [[2, 3], [4, 5]])
 
 # Or update a single cell
-wks.update('B42', "it's down there somewhere, let me take alook.")
+wks.update('B42', "it's down there somewhere, let me take a look.")
 
 # Format the header
 wks.format('A1:B1', {'textFormat': {'bold': True}}) 
